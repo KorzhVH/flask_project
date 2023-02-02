9383002bafe377c3fb99ddb1279a24d1e5aad006
VladyslavKorzh <vladyslav.korzh@dev.pro>
2023-02-02 14:15:32 +0200

    base methods created

--- main.py
+++ main.py
@@ -3,56 +3,56 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/vacancy/")
+@app.route("/vacancy/", methods=['GET', 'POST'])
 def vacancy():
     return 'any vacancy'
 
 
-@app.route("/vacancy/id/")
+@app.route("/vacancy/id/", methods=['GET', 'DELETE'])
 def vacancy_id():
     return 'vacancy by id'
 
 
-@app.route("/vacancy/id/events/")
+@app.route("/vacancy/id/events/", methods=['GET', 'POST', 'DELETE'])
 def vacancy_events():
     return 'show all events'
 
 
-@app.route("/vacancy/id/events/event_id/")
+@app.route("/vacancy/id/events/event_id/", methods=['GET', 'PUT', 'DELETE'])
 def show_event_by_id():
     return 'show event by id'
 
 
-@app.route("/vacancy/id/history/")
+@app.route("/vacancy/id/history/", methods=['GET', 'DELETE'])
 def vacancy_history():
     return 'any vacancy'
 
 
-@app.route("/user/")
+@app.route("/user/", methods=['GET'])
 def get_user_main_page():
     return 'any vacancy'
 
 
-@app.route("/user/calendar/")
+@app.route("/user/calendar/", methods=['GET'])
 def user_calendar():
     return 'show user calendar'
 
 
-@app.route("/user/mail/")
+@app.route("/user/mail/", methods=['GET'])
 def user_mail():
     return 'show users mail box '
 
 
-@app.route("/user/settings/")
+@app.route("/user/settings/", methods=['GET', 'PUT'])
 def user_settings():
     return 'any vacancy'
 
 
-@app.route("/user/documents/")
+@app.route("/user/documents/", methods=['GET', 'POST', 'PUT', 'DELETE'])
 def user_documents():
     return 'show user documents'
 
 
-@app.route("/user/templates/")
+@app.route("/user/templates/", methods=['GET', 'POST', 'PUT', 'DELETE'])
 def user_templates():
     return 'users templates'
