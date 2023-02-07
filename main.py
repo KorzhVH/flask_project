dfbdc56c81ab5e06717c89772b3d1fecf1e2e4a2
Vladyslav Korzh <tastetesting190599kvg@gmail.com>
2023-02-07 04:01:45 +0200

    Hometask 2 finished, changed name for project

--- main.py
+++ main.py
@@ -2,25 +2,94 @@ from flask import Flask
 
 app = Flask(__name__)
 
+vacancy_data = [
+    {"id": 1,
+     "creation_date": "01.01.2023",
+     "status": 1,
+     "company": "some company",
+     "contacts_id": [1, 2],
+     "description": "It's something",
+     "position_name": "Python Developer",
+     "comment": "some comment",
+     "user_id": 101
+     },
+    {"id": 2,
+     "creation_date": "15.01.2023",
+     "status": 1,
+     "company": "some company",
+     "contacts_id": [3, 4],
+     "description": "It's something",
+     "position_name": "Python Developer",
+     "comment": "some comment",
+     "user_id": 102
+     },
+    {"id": 3,
+     "creation_date": "01.02.2023",
+     "status": 1,
+     "company": "some company",
+     "contacts_id": [5, 6],
+     "description": "It's something",
+     "position_name": "Python Developer",
+     "comment": "some comment",
+     "user_id": 103
+     }
+]
+events_data = [
+    {"id": 1,
+     "vacancy_id": 1,
+     "description": "It's something",
+     "event_date": "01.01.2024",
+     "title": "Test Title",
+     "due_to_date": "01.01.2030",
+     "status": 1
+     },
+    {"id": 2,
+     "vacancy_id": 2,
+     "description": "It's something",
+     "event_date": "15.01.2024",
+     "title": "Test Title",
+     "due_to_date": "01.01.2030",
+     "status": 1
+     },
+    {"id": 3,
+     "vacancy_id": 3,
+     "description": "It's something",
+     "event_date": "01.02.2025",
+     "title": "Test Title",
+     "due_to_date": "01.01.2030",
+     "status": 1
+     }
+]
+
 
 @app.route("/vacancy/", methods=['GET', 'POST'])
 def vacancy():
-    return 'any vacancy'
+    return vacancy_data
 
 
-@app.route("/vacancy/id/", methods=['GET', 'DELETE'])
-def vacancy_id():
-    return 'vacancy by id'
+@app.route("/vacancy/vacancy_id/", methods=['GET', 'DELETE'])
+def vacancy_id(vacancy_id):
+    for vacancy_searcher in vacancy_data:
+        if vacancy_searcher["vacancy_id"] == vacancy_id:
+            return vacancy_searcher
 
 
-@app.route("/vacancy/id/events/", methods=['GET', 'POST', 'DELETE'])
-def vacancy_events():
-    return 'show all events'
+@app.route("/vacancy/vacancy_id/events/", methods=['GET', 'POST', 'DELETE'])
+def vacancy_events(vacancy_id):
+    list_of_events_for_vacancy = []
+    for event_searcher in events_data:
+        if event_searcher["vacancy_id"] == vacancy_id:
+            list_of_events_for_vacancy.append(event_searcher)
+    return list_of_events_for_vacancy
 
 
-@app.route("/vacancy/id/events/event_id/", methods=['GET', 'PUT', 'DELETE'])
-def show_event_by_id():
-    return 'show event by id'
+@app.route("/vacancy/vacancy_id/events/event_id/", methods=['GET', 'PUT', 'DELETE'])
+def show_event_by_id(vacancy_id, event_id):
+    list_of_events_for_vacancy = []
+    for event_searcher in events_data:
+        if event_searcher["vacancy_id"] == vacancy_id:
+            list_of_events_for_vacancy.append(event_searcher)
+    return list_of_events_for_vacancy[event_id]
 
 
 @app.route("/vacancy/id/history/", methods=['GET', 'DELETE'])
