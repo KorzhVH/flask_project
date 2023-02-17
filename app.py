ea47c7bfc89e10ae07ec1225c85241261e874fbc
Vladyslav Korzh <tastetesting190599kvg@gmail.com>
2023-02-17 16:09:11 +0200

    updates for bd

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import db_processing
 
 app = Flask(__name__)
 
-events_data={
+events_data = {
 
 }
+
+
 @app.route("/vacancy/", methods=['GET', 'POST'])
 def vacancy():
     if request.method == "POST":
@@ -34,10 +36,28 @@ def vacancy():
 
 
 @app.route("/vacancy/<vacancy_id>/", methods=['GET', 'PUT'])
-def vacancy_id():
+def vacancy_id(vacancy_id):
     if request.method == 'PUT':
-        db_processing.insert_info("Select * from vacancy where vacancy_id = '%s")%(vacancy_id)
-    return render_template('add-vacancy.html', all_vacancies=result)
+        position_name = request.form.get('position_name')
+        company = request.form.get('company')
+        description = request.form.get('description')
+        contacts_id = request.form.get('contacts_id')
+        comment = request.form.get('comment')
+        table_data = {
+            "vacancy_id": 1,
+            "creation_date": "01-01-2022",
+            "position_name": position_name,
+            "company": company,
+            "description": description,
+            "contacts_id": contacts_id,
+            "comment": comment,
+            "user_id": 1
+        }
+        query = f'UPDATE vacancy SET position_name = {position_name}, company = {company}, description = {description}, contacts_id={contacts_id}, comment = {comment}'
+        db_processing.insert_info(query, table_data)
+    result = db_processing.select_info('Select * from vacancy where vacancy_id = %s' % vacancy_id)
+    return render_template('add-vacancy.html', all_vacancies=result)  # I think here should be different html template
+    # for  put form. Please tell me if I'm wrong.
 
 
 @app.route("/vacancy/<vacancy_id>/events/", methods=['GET', 'POST'])
@@ -47,7 +67,7 @@ def vacancy_events(vacancy_id):
         event_date = request.form.get('event_date')
         title = request.form.get('title')
         due_to_date = request.form.get('due_to_date')
-        event_data= {
+        event_data = {
             'event_id': 1,
             'vacancy_id': 1,
             'description': description,
@@ -55,7 +75,7 @@ def vacancy_events(vacancy_id):
             'title': title,
             'due_to_date': due_to_date,
             'status': 1
-            }
+        }
         db_processing.insert_info('events', event_data)
     result = db_processing.select_info('Select * from events')
     return render_template('add_event.html', all_events=result)
